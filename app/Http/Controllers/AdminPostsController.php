3468cb12886606bf15b7a9fcc7629c85277fada4
3A532081 <tanshui601@gmail.com>
2018-12-06 19:20:47 +0800

    練習4-4：設定 AdminPostsController對應的 function

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -28,4 +28,10 @@ class AdminPostsController extends Controller
 
         return view('admin.posts.edit', $data);
     }
+
+    public function store(PostRequest $request)
+    {
+        Post::create($request->all());
+        return redirect()->route('admin.posts.index');
+    }
 }
